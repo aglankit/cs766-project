102662a8b9b27faad3f5ae5d25b360e50ce582d5
Ankit Agrawal <agrawal.1503@gmail.com>
2016-04-26 14:15:50 -0500

    Cleaning

--- applyHomography.m
+++
@@ -1,7 +0,0 @@
-function dest_pts_nx2 = applyHomography(H_3x3, src_pts_nx2)
-num_points = size(src_pts_nx2, 1);
-inp_mat = [src_pts_nx2, ones(num_points,1)]';
-temp = (H_3x3 * inp_mat);
-temp(1,:) = temp(1,:)./temp(3,:);
-temp(2,:) = temp(2,:)./temp(3,:);
-dest_pts_nx2 = (temp(1:2, :))';
