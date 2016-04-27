44f2273d20fd951182fbe60574efed0d7b37f4c2
achinkulshrestha <arcaneak@email.com>
2016-04-27 16:25:43 -0500

    Added mapper for depth to color

--- contruct3dImageScene.m
+++ contruct3dImageScene.m
@@ -1,4 +1,4 @@
-function contruct3dImageScene()
+function [res] = contruct3dImageScene()
 colorDevice = imaq.VideoDevice('kinect',1);
 depthDevice = imaq.VideoDevice('kinect',2);
 
@@ -38,3 +38,4 @@ view(player,ptCloud);
 
 release(colorDevice);
 release(depthDevice);
+res = 0;
---
+++ csharp2.m
@@ -0,0 +1,7 @@
+function [b, d] = scharp2(x, y)
+%CSHARPTEST Summary of this function goes here
+%   Detailed explanation goes here
+b = x + y; 
+d = sprintf('Hello this is the return%s',b); 
+end
+
